c7fb0f60d7670b998112e1d405e3657ed231e202
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2025-08-02 15:42:17 +0000

    basics

--- 01-basics/04_Comparisons.js
+++ 01-basics/04_Comparisons.js
@@ -1,21 +1,20 @@
-// console.log(2 > 1);
+// console.log(2 > 1); output boolean mein aayega "true"
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
+// console.log("2" > 1); ****Typescript do not allow to compare differnet datatypes******
 // console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
-console.log(null >= 0);
-
+console.log(null >= 0); // ### line 12 null ko "0" allot karwa diya hai tho false aayega###
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); // ### strict check
