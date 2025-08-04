3b13004f49948e89ddeab7f959b919d5a21b0a67
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2025-08-04 13:06:41 +0000

    Basics01

--- 01-basics/07_datesinJs.js
+++ 01-basics/07_datesinJs.js
@@ -1,10 +1,10 @@
-// Dates
+// Dates  arbitrary date "1st jan 1970"
 
 let myDate = new Date()
-// console.log(myDate.toString());
+// console.log(myDate.toString());  kafi better hai
 // console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(myDate.toLocaleString()); time bhi show hoga
+// console.log(typeof myDate);  object type
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
@@ -16,11 +16,11 @@ let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000)); converted millsecond to second
 
 let newDate = new Date()
 console.log(newDate);
-console.log(newDate.getMonth() + 1);
+console.log(newDate.getMonth() + 1); // 0 se strt hoga isiliya +1
 console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
@@ -29,3 +29,4 @@ newDate.toLocaleString('default', {
     weekday: "long",
     
 })
+
